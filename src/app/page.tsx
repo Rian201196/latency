67c939492f21cf95ec0f9c59c3fa09b369b02463
Rian201196 <rian8pratama@gmail.com>
2024-11-06 16:13:36 +0700

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import dynamic from "next/dynamic";
 import Image from "next/image";
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+
+
 
 const Map = dynamic(() => import("@/components/map/latency"), { ssr: false });
 
@@ -10,26 +13,49 @@ export default function HomePage() {
         <div className="relative sm:col-span-9 sm:row-span-5 min-h-[100px] rounded-lg overflow-hidden">
           <Map center={[-6.155158139563846, 105.50465998162689]} zoom={7} />
         </div>
-        <div id="cetak" className="sm:col-span-3 sm:row-span-1 h-[5rem] rounded-lg relative">
+        <div
+          id="cetak"
+          className="sm:col-span-3 sm:row-span-1 h-[5rem] rounded-lg relative"
+        >
           <Image
             src="/cetak.png"
             alt="Cetak Image"
-            // layout="fill"
-            objectFit="cover"
-            className="rounded-lg absolute bottom-0 right-0"
-            width={100} 
-            height={50} 
+            className="rounded-lg absolute bottom-0 right-0 object-cover"
+            width={160}
+            height={50}
+            priority
+            style={{ width: "auto", height: "auto" }}
           />
+        </div>
+        <div
+          id="aktif"
+          className="sm:col-span-3 sm:row-span-1 h-[5rem] rounded-lg bg-[#d9fce4] opacity-70 p-2"
+        >
+          <div className="grid grid-cols-4 h-full items-center gap-2">
+            <div className="icon col-span-1 text-center">
+            <RadioButtonCheckedIcon sx={{ fontSize: 60, color: '#66ff66' }} />
+            </div>
+            <div className="status col-span-2 text-center">
+            <h1 className="font-roboto text-xl font-bold">JUMLAH SENSOR ON</h1>
+            </div>
+            <h1 className="font-roboto text-2xl font-bold">150</h1>
+          </div>
         </div>
 
         <div
-          id="aktif"
-          className="sm:col-span-3 sm:row-span-1 h-[5rem] rounded-lg bg-[#d9fce4] opacity-70"
-        ></div>
-        <div
           id="mati"
-          className="sm:col-span-3 sm:row-span-1 h-[5rem] rounded-lg bg-[#feeeee] opacity-70"
-        ></div>
+          className="sm:col-span-3 sm:row-span-1 h-[5rem] rounded-lg bg-[#feeeee] opacity-70 p-2"
+        >
+          <div className="grid grid-cols-4 h-full items-center gap-2">
+            <div className="icon col-span-1 text-center">
+            <RadioButtonCheckedIcon sx={{ fontSize: 60, color: '#ff6666' }} />
+            </div>
+            <div className="status col-span-2 text-center">
+            <h1 className="font-roboto text-xl font-bold">JUMLAH SENSOR OFF</h1>
+            </div>
+            <h1 className="font-roboto text-2xl font-bold">150</h1>
+          </div>
+        </div>
         <div
           id="popup"
           className="sm:col-span-3 sm:row-span-3 min-h-[100px] rounded-lg bg-white opacity-90"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -78,3 +81,4 @@ const config = {
 } satisfies Config
 
 export default config
+
